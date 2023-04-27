d85ce3686482130380d48f6a01e7e6e5743cdd29
rashedrk <rashedrk001@gmail.com>
2023-04-27 16:34:55 +0600

    Set Auth Context and Auth Provider to share auth info

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from "../../../assets/logo.png";
 import moment from 'moment';
 import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FaUserCircle } from "react-icons/fa";
+import { AuthContext } from '../../../Providers/AuthProvider/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     return (
         <Container>
             <div className="text-center">
@@ -30,9 +34,16 @@ const Header = () => {
                             <Nav.Link href="#">Career</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">profile</Nav.Link>
-                            <Nav.Link href="#memes">
-                                <Button variant="dark">Login</Button>
+                            {user && <Nav.Link>
+                                <FaUserCircle style={{ fontSize: '2rem' }} />
+                                <span className='ms-2'>{user.name}</span>
+                            </Nav.Link>}
+                            <Nav.Link>
+                                {user ?
+                                    <Button variant="dark">Logout</Button>
+                                    :
+                                    <Link to='/login'><Button variant="dark">Login</Button></Link>
+                                    }
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
